3c57728fa59e529a92efb0c9783593fa92295ad8
Ivan <colo9311@gmail.com>
2016-11-28 20:24:00 -0300

    Eliminados campos code en los modelos de la app usuario.

--- usuario/models.py
+++ usuario/models.py
@@ -5,7 +5,6 @@ from django.db import models
 # Create your models here.
 
 class TypeUser(models.Model):
-    codigo = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=100, verbose_name='Nombre')
 
 
