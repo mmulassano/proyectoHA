d026a82003e5b7b4602e4043174bd7635e5d990b
Ivan <colo9311@gmail.com>
2016-11-28 20:11:13 -0300

    Prefijo id eliminado en los modelos de la app mapeo.

--- mapeo/models.py
+++ mapeo/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    id_user = models.IntegerField()
-    mobile_id = models.IntegerField(unique=True)
+    user = models.IntegerField()
+    mobile = models.IntegerField(unique=True)
 
 
 ##Modelos para realizar MAPEO
@@ -15,31 +15,31 @@ class Post(models.Model):
 #Farm (campos)
 class Farm(models.Model):
     #id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField()
+    user = models.IntegerField()
     name = models.CharField(max_length=100)
     active = models.BooleanField(default=True)
-    movil_id = models.BigIntegerField(unique=True)
+    movil = models.BigIntegerField(unique=True)
     #dateLastModification = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-	return self.name
-    #def validate(self, request):
-    #    key = request.user
-    #    raise serializers.ValidationError(("nada"))
+        return self.name
+        #def validate(self, request):
+        #    key = request.user
+        #    raise serializers.ValidationError(("nada"))
 
 
 
 #Parcel (parcelas)
 class Parcel(models.Model):
     #id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField()
-    farm_id = models.BigIntegerField()
+    user = models.IntegerField()
+    farm = models.BigIntegerField()
     name = models.CharField(max_length=100)
     surface = models.DecimalField(max_digits=10, decimal_places=2)
     active = models.BooleanField(default=True)
     center_lat = models.CharField(max_length=100)
     center_long = models.CharField(max_length=100)
-    movil_id = models.BigIntegerField(unique=True)
+    movil = models.BigIntegerField(unique=True)
     
     def __unicode__(self):
         return self.name
@@ -47,13 +47,13 @@ class Parcel(models.Model):
 
 #Actividades en parcelas
 class Activity(models.Model):
-    user_id = models.IntegerField()
-    parcel_id = models.BigIntegerField()
-    type_crop_id = models.IntegerField()
+    user = models.IntegerField()
+    parcel = models.BigIntegerField()
+    type_crop = models.IntegerField()
     campaign = models.CharField(max_length=100)
     date = models.CharField(max_length=100)
     active = models.BooleanField(default=True)
-    movil_id = models.BigIntegerField(unique=True)
+    movil = models.BigIntegerField(unique=True)
 
     def __unicode__(self):
         return self.campaing
