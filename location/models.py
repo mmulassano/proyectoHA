7b834b60f80fcb2a42a02f0351c3fd849a569e56
Ivan <colo9311@gmail.com>
2016-11-28 20:09:25 -0300

    Prefijo id eliminado en los modelos de la app location.

--- location/models.py
+++ location/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    user_id = models.IntegerField()
+    user = models.IntegerField()
     type_object = models.CharField(max_length=100)  #parcel,farm,user
-    object_id = models.BigIntegerField()
+    object = models.BigIntegerField()
     order = models.IntegerField()
     lat = models.CharField(max_length=100)
     long = models.CharField(max_length=100)
 
     def __unicode__(self):
-	return self.type_object
+        return self.type_object
 
 
