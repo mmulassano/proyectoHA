ad78dfc00d56a1cf82c0bd4cdd1b6ca4c02632c7
Ivan <colo9311@gmail.com>
2016-11-28 21:45:05 -0300

    Campos de tipo entero cambiados a tipo ForeignKey en los modelos de la app location.

--- location/models.py
+++ location/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Location(models.Model):
-    user = models.IntegerField()
+    user = models.ForeignKey(User)
     type_object = models.CharField(max_length=100)  #parcel,farm,user
     object = models.BigIntegerField()
     order = models.IntegerField()
