8ec9b8fe2798fc7daa08227a5892071dbc7f6258
Ivan <colo9311@gmail.com>
2016-11-28 21:48:55 -0300

    Campos de tipo entero cambiados a tipo ForeignKey en los modelos de la app pushes.

--- pushes/models.py
+++ pushes/models.py
@@ -1,13 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Device(models.Model):
     #name = models.CharField(max_length=100)
     active = models.BooleanField(default=True)
-    user = models.IntegerField()
+    user = models.ForeignKey(User)
     #device_id = models.IntegerField()
     key_firebase = models.CharField(max_length=500)
     #date_create = models.CharField(max_length=100)
